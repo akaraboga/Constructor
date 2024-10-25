50f9d7f602444d9540f9f584c10c8298c0c29806
Ahmet Karaboğa <130494000+akaraboga@users.noreply.github.com>
2024-10-25 10:40:00 +0300

    Add files via upload

---
+++ Constructor/Constructor/Bebe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Bebe
+    {
+        public string Ad { get; set; }//Bebeğin propertyleri
+        public string SoyAd { get; set; }
+        public DateTime Dtarih { get; set; }
+
+        public Bebe()//Parametre almayan Constructor 
+        {
+             
+            Dtarih = DateTime.Now;
+
+        }
+
+        public Bebe(string ad, string soyad)//Parametre alan Constructor
+        {
+            Ad = ad;//Propertylerimizi gelecek olan  parametre  değere atıyoruz
+            SoyAd = soyad;
+         
+            Dtarih = DateTime.Now;
+            
+        }
+
+        public void BebeBilgi()
+        {
+            Console.WriteLine("Adı Soyadı : {0} {1} Ingaa doğum tarihi : {2} ", Ad, SoyAd, Dtarih);
+        }
+
+
+    }
+
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bebe bebek = new Bebe("Ahmet","Karaboğa");//Değer alan Constructorın içine new ile ad,soyad bilgisini gonderiyoruz
+            bebek.BebeBilgi();
+
+            Bebe Bebek2 = new Bebe();//Değer almayan Constructorı Bebek2 nesnesine atayarak propertylerine tek tek veri gonderiyoruz.
+            Bebek2.Ad = "Ayşe";
+            Bebek2.SoyAd = "Kara";
+            Bebek2.BebeBilgi();
+
+            Console.ReadLine(); 
+        }
+    }
+}
